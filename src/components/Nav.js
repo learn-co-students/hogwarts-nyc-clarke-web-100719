a1645e6b08fc1f90dfbb4c214e5a05f32de93aaf
gnardinosaur <github email address>
2019-12-13 08:34:02 -0500

    Done.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import piggy from '../porco.png'
 import React from 'react'
 
-const Nav = () => {
+const Nav = (props) => {
 	return (
 		<div className="navWrapper">
 			<span className="headerText">Hogwarts</span>
@@ -9,6 +9,12 @@ const Nav = () => {
 				<img src={piggy} className="App-logo" alt="piggy" />
 			</div>
 			<span className="normalText">A React App for County Fair Hog Fans</span>
+			<button className="greased" onClick={props.filterHogs}>Greased?</button>
+			<select onChange={props.sortHogs} >
+				<option>Sort By:</option>
+				<option value="name">Name</option>
+				<option value="weight">Weight</option>
+			</select>
 		</div>
 	)
 }
